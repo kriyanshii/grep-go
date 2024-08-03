165e49c811677b2d3b80af7ce61ea745a305d1ca
kriyanshi <kriyanshishah06@gmail.com>
2024-08-03 21:25:12 +0530

    codecrafters submit [skip ci]

--- cmd/mygrep/main.go
+++ cmd/mygrep/main.go
@@ -3,11 +3,12 @@ package main
 import (
 	// Uncomment this to pass the first stage
 	// "bytes"
-	"bytes"
+
 	"fmt"
 	"io"
 	"os"
-	"unicode/utf8"
+	"strings"
+	"unicode"
 )
 
 // Usage: echo <input_text> | your_program.sh -E <pattern>
@@ -25,7 +26,7 @@ func main() {
 		os.Exit(2)
 	}
 
-	ok, err := matchLine(line, pattern)
+	ok, err := matchLine(string(line), pattern)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(2)
@@ -38,31 +39,79 @@ func main() {
 	// default exit code is 0 which means success
 }
 
-func matchLine(line []byte, pattern string) (bool, error) {
-	if utf8.RuneCountInString(pattern) == 0 {
-		return false, fmt.Errorf("unsupported pattern: %q", pattern)
-	}
+// func matchLine(line string, pattern string) (bool, error) {
+// 	if utf8.RuneCountInString(pattern) == 0 {
+// 		return false, fmt.Errorf("unsupported pattern: %q", pattern)
+// 	}
 
-	var ok bool
+// 	var ok bool
 
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+// 	// You can use print statements as follows for debugging, they'll be visible when running tests.
+// 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this to pass the first stage
-	if pattern == "\\d" {
-		ok = bytes.ContainsAny(line, "1234567890")
-	} else if pattern == "\\w" {
-		ok = bytes.ContainsAny(line, "abcdefghijklmnopqrstuvwxyz")
-	} else if pattern[0] == '[' && pattern[len(pattern)-1] == ']' {
-		n := len(pattern)
-		if pattern[1] == '^' {
-			ok = !bytes.ContainsAny(line, pattern[2:n-1])
-		} else {
-			ok = bytes.ContainsAny(line, pattern[1:n-1])
+// 	// Uncomment this to pass the first stage
+// 	if pattern == "\\d" {
+// 		ok = bytes.ContainsAny(line, "1234567890")
+// 	} else if pattern == "\\w" {
+// 		ok = bytes.ContainsAny(line, "abcdefghijklmnopqrstuvwxyz")
+// 	} else if pattern[0] == '[' && pattern[len(pattern)-1] == ']' {
+// 		n := len(pattern)
+// 		if pattern[1] == '^' {
+// 			ok = !bytes.ContainsAny(line, pattern[2:n-1])
+// 		} else {
+// 			ok = bytes.ContainsAny(line, pattern[1:n-1])
+// 		}
+// 	} else {
+// 		ok = bytes.ContainsAny(line, pattern)
+// 	}
+
+// 	return ok, nil
+// }
+
+func matchLine(line string, pattern string) (bool, error) {
+	for i := 0; i < len(line); i++ {
+		if matchPattern(line, pattern, i) {
+			return true, nil
 		}
-	} else {
-		ok = bytes.ContainsAny(line, pattern)
 	}
+	return false, nil
+}
 
-	return ok, nil
+func matchPattern(line string, pattern string, pos int) bool {
+	n := len(pattern)
+	j := pos
+	for i := 0; i < n; i++ {
+		if j >= len(line) {
+			return false
+		}
+		if pattern[i] == '\\' && i+1 < n {
+			if pattern[i+1] == 'd' && !unicode.IsDigit(rune(line[j])) {
+				return false
+			} else if pattern[i+1] == 'w' && !unicode.IsLetter(rune(line[j])) {
+				return false
+			} else {
+				i++
+			}
+		} else if pattern[i] == '[' && i+1 < n && pattern[i+1] == '^' {
+			endPos := strings.Index(pattern[i:], "]")
+			matchThisPattern := pattern[i:endPos]
+			if strings.Contains(matchThisPattern, string(line[j])) {
+				return false
+			}
+			i = endPos
+		} else if pattern[i] == '[' {
+			endPos := strings.Index(pattern[i:], "]")
+			matchThisPattern := pattern[i:endPos]
+			if !strings.Contains(matchThisPattern, string(line[j])) {
+				return false
+			}
+			i = endPos
+		} else {
+			if line[j] != pattern[i] {
+				return false
+			}
+		}
+		j++
+	}
+	return true
 }
